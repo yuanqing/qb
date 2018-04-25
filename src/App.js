fbdd6cba1716fb76f2283c54d0f9baae919a8178
Yuan Qing <desk@yuanqing.sg>
2018-04-25 23:01:25 +0800

    Abstraction

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 const React = require('react')
-const BarCode = require('./BarCode')
-const QrCode = require('./QrCode')
+const Codes = {
+  BarCode: require('./BarCode'),
+  QrCode: require('./QrCode')
+}
 const data = require('./data')
 
 class App extends React.Component {
   render () {
     return (
       <div className='App'>
-        <div className='Page'>
-          <h1>Products</h1>
-          {data.products.map(function (value) {
-            return <BarCode value={value} />
-          })}
-        </div>
-        <div className='Page'>
-          <h1>Baskets</h1>
-          {data.baskets.map(function (value) {
-            return <QrCode value={value} />
-          })}
-        </div>
-        <div className='Page'>
-          <h1>Tote bins</h1>
-          {data.toteBins.map(function (value) {
-            return <QrCode value={value} />
-          })}
-        </div>
-        <div className='Page'>
-          <h1>Shelves</h1>
-          {data.shelves.map(function (value) {
-            return <QrCode value={value} />
-          })}
-        </div>
+        {Object.keys(data).map(function (title) {
+          const { codes, type } = data[title]
+          const Code = Codes[type]
+          return (
+            <div className='Page'>
+              <h1>{title}</h1>
+              {codes.map(function (value) {
+                return <Code value={value} />
+              })}
+            </div>
+          )
+        })}
       </div>
     )
   }
