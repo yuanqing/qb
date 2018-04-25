45db09c90cc2b33cd76a01140b2d7be2e8277868
Shiny <contact@shinychang.net>
2018-04-25 18:32:12 +0800

    Update data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-const React = require('react')
-const BarCode = require('./BarCode')
-const QrCode = require('./QrCode')
-const data = require('./data')
+const React = require('react');
+const BarCode = require('./BarCode');
+const QrCode = require('./QrCode');
+const data = require('./data');
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='App'>
-        <div className='Page'>
+      <div className="App">
+        <div className="Page">
           <h1>Products</h1>
-          {data.products.map(function (value) {
-            return <BarCode value={value} />
+          {data.products.map(function(value) {
+            return <BarCode value={value} />;
           })}
         </div>
-        <div className='Page'>
+        <div className="Page">
           <h1>Baskets</h1>
-          {data.baskets.map(function (value) {
-            return <QrCode value={value} />
+          {data.baskets.map(function(value) {
+            return <QrCode value={value} />;
           })}
         </div>
-        <div className='Page'>
+        <div className="Page">
+          <h1>Tote bins</h1>
+          {data.toteBins.map(function(value) {
+            return <QrCode value={value} />;
+          })}
+        </div>
+        <div className="Page">
           <h1>Shelves</h1>
-          {data.shelves.map(function (value) {
-            return <QrCode value={value} />
+          {data.shelves.map(function(value) {
+            return <QrCode value={value} />;
           })}
         </div>
       </div>
-    )
+    );
   }
 }
 
-module.exports = App
+module.exports = App;
